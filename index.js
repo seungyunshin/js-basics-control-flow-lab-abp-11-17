1488464ed08b63c0cabe0259ffea418f12273665
Seung-Yun <sseungyun@gmail.com>
2017-11-13 06:50:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function scuberGreetingForFeet(feet){
 function ternaryCheckCity(city){
   if (city = "NYC"){
   return "Ok, sounds good."
-  } else {
+} if (city != "NYC") {
   return "No go."
   }
 }
