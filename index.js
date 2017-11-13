ec6ecffdda35910da445769e3c75f0a382a83bfc
Seung-Yun <sseungyun@gmail.com>
2017-11-13 07:15:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function ternaryCheckCity(city){
 let switchOnCharmFromTip;
 let tip;
 
-switch (tip){
+switch (switchOnCharmFromTip()){
   case "generous":
     switchOnCharmFromTip = "Thank you so much.";
     break;
