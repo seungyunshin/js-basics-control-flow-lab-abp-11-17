543a730e4ac64b5f5c1600502366e45a83f678a7
Seung-Yun <sseungyun@gmail.com>
2017-11-13 06:48:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 // Write your code in this file!
 function scuberGreetingForFeet(feet){
-  if (feet <= 400)
+  if (feet <= 400){
   return "This one is on me!"
-  if (feet > 400 && feet <= 2500)
+  }
+  if (feet > 400 && feet <= 2500){
   return "I will gladly take your thirty bucks."
-  if (feet > 2500)
+  }
+  if (feet > 2500){
   return "No can do."
+  }
 }
 
 function ternaryCheckCity(city){
-  if (city = "NYC")
+  if (city = "NYC"){
   return "Ok, sounds good."
-
+  }
   else {
   return "No go."
   }
