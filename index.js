472cd8e5f532d058cc63a96fa436ae4f01e7fe75
Seung-Yun <sseungyun@gmail.com>
2017-11-13 07:37:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+  return "This one is on me!"
+  }
+  if (feet > 400 && feet <= 2500){
+  return "I will gladly take your thirty bucks."
+  }
+  if (feet > 2500){
+  return "No can do."
+  }
+}
+
+function ternaryCheckCity(city){
+  if (city === "NYC"){
+  return "Ok, sounds good."
+  } else {
+  return "No go."
+  }
+}
+
+
+function switchOnCharmFromTip(tip){
+switch (tip){
+  case 'generous':
+    return 'Thank you so much.';
+
+  case 'not as generous':
+    return 'Thank you.';
+
+  default:
+    return 'Bye.';
+    
+  }
+}
+switchOnCharmFromTip();
