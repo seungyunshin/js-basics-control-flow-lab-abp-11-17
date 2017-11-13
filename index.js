dd8aaccd097986bc93bcac491bf9f04b1c2e9dee
Seung-Yun <sseungyun@gmail.com>
2017-11-13 07:35:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ function switchOnCharmFromTip(tip){
 switch (tip){
   case 'generous':
     return 'Thank you so much.';
-    break;
+
   case 'not as generous':
     return 'Thank you.';
-    break;
+
   default:
     return 'Bye.';
-    break;
+    
   }
 }
 switchOnCharmFromTip();
