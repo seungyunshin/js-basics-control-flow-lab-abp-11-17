ff5d326849e0ad10c05a906366d49dd399a98061
Seung-Yun <sseungyun@gmail.com>
2017-11-13 06:43:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 function scuberGreetingForFeet(feet){
   if (feet <= 400)
   return "This one is on me!"
-  if (feet > 400 && feet < 2500)
+  if (feet > 400 && feet <= 2500)
   return "I will gladly take your thirty bucks."
+  if (feet > 2500)
+  return "No can do."
 }
